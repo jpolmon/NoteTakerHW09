095854e43dca0f201acf9641bc0b69216afb0e03
Jonathan Olmon <jonathan.olmon@gmail.com>
2021-09-01 14:29:08 -0500

    Attempting to fix port.

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,7 @@
 const note = require('express').Router();
-const { readFromFile, readAndAppend, readAndDelete } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
+const { readFromFile, readAndAppend, readAndDelete } = require('../helpers/fsUtils');
+
 
 note.get('/', (req, res) => {
     console.info(`${req.method} request received for notes`);
